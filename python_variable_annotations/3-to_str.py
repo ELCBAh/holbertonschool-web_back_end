88ba6813985419661c9960b2a0f5d9bd8f0d098c
ELCBAh <6211@holbertonstudents.com>
2023-08-29 22:19:34 +0000

    fixed pycodestyle errors in to str file

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """
-type-annotated function to_str that takes a float n as argument and returns the string representation of the float.
+type-annotated function to_str that takes a float
+n as argument and returns the string representation of the float.
 """
 
 
 def to_str(n: float) -> str:
     """
-    takes a float n as argument and returns the string representation of the float.
+    takes a float n as argument and returns the string
+    representation of the float.
     """
     return str(n)
