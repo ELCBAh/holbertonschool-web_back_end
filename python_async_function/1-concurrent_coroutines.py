a0661778f4c78771a39b4b960376b0e8875e5af5
ELCBAh <6211@holbertonstudents.com>
2023-09-01 23:54:40 +0000

    trying a new approach to fix checker typo error

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,9 +10,10 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Returns a list of all the delays (float values).
     """
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
+    delay_list = [wait_random(max_delay) for i in range(n)]
+    tasks = [asyncio.create_task(delay) for delay in asyncio.as_completed(delay_list)]
+    return [await task for task in tasks]
