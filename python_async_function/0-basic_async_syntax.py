5768ded243b9fb96ff68b41a21b77d3e1c89c98b
ELCBAh <6211@holbertonstudents.com>
2023-09-01 23:26:48 +0000

    fixed function not waiting for delay

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,8 +4,11 @@ async coroutine that takes int as argument and returns float
 """
 import asyncio
 from typing import Generator
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """Wait for a random delay between 0 and max_delay."""
-    return max_delay
+    delay = max_delay * random.random()
+    await asyncio.sleep(delay)
+    return delay
