b9930a4c34f463c1644a320233c2295428227c63
ELCBAh <6211@holbertonstudents.com>
2023-09-02 22:54:26 +0000

    fixed checker and removed unnecessary libraries

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -4,7 +4,6 @@ takes integer max_delay and returns the time it takes to execute
 """
 import asyncio
 import random
-from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
@@ -14,4 +13,4 @@ async def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     task_wait_random function
     """
-    return await wait_random(max_delay)
+    return asyncio.create_task(wait_random(max_delay))
