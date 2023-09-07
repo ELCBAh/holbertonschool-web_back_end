1857add4e8257906ebce50d5457f01c0b4d62a15
ELCBAh <6211@holbertonstudents.com>
2023-09-07 16:10:29 +0000

    all excercise added, pycodestyle fixed and given exec permissions

---
+++ NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This code will list all docs in a mongodb collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    list all documents in a collection
+    """
+    return mongo_collection.find() or []
+
+
+if __name__ == "__main__":
+    list_all(mongo_collection):
