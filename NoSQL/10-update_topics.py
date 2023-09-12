f1c4084f0757be5d9dcbceab1fe5e9dad5406bcd
ELCBAh <6211@holbertonstudents.com>
2023-09-12 20:04:02 +0000

    update topics excercise added and given exec permissions

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This code will update a school document in the schools collection
+"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    returns updated school document
+    """
+    return mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
