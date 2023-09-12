be02c79fa962a33a8820c1c3bb490b7262d4dcd6
ELCBAh <6211@holbertonstudents.com>
2023-09-12 22:00:03 +0000

    fixed format to print as expected

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
     for method in methods:
-        print(f"\tmethod: {method}: \
-            {collection.count_documents({'method': method})} {method}")
+        print(f"\tmethod {method}: \
+            {method} {collection.count_documents({'method': method})}")
     print(f"{collection.count_documents({'method': 'GET', 'path': '/status'})}\
-        status check")
+         status check")
     client.close()
