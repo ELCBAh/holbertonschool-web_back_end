84647c36547b14eef60642d9cec64a9b54c01b55
ELCBAh <6211@holbertonstudents.com>
2023-09-12 21:01:25 +0000

    fixed format to print as expected by checker

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -12,8 +12,9 @@ if __name__ == "__main__":
     collection = db.nginx
     print(f"{collection.count_documents({})} logs")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
     for method in methods:
-        print(f"{collection.count_documents({'method': method})} {method}")
-    print(f"{collection.count_documents({'method': 'GET', 'path': '/status'})}\
+        print(f"{collection.count_documents({'\tmethod': method})} {method}")
+    print(f"{collection.count_documents({'\tmethod': 'GET', 'path': '/status'})}\
         GET /status")
     client.close()
