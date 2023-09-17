d6ab991dc0b2f03bab2da0a3eb7dc5e2e41385e7
ELCBAh <6211@holbertonstudents.com>
2023-09-17 16:02:29 +0000

    fixed lint errors

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (let value of array) {
-    array.push(appendString + value);
+  for (let value of array) {
+  array.push(appendString + value);
   }
 
   return array;
