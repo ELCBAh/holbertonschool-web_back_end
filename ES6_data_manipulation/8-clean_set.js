f98cd678b324a53a6d690894b1a3de9f4d2328f3
ELCBAh <6211@holbertonstudents.com>
2024-02-06 01:58:57 +0000

    fixing lint in excercise 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@ const cleanSet = (set, startString) => {
 
   set.forEach((value) => {
     if (value.startsWith(startString)) {
-      result += value.slice(startString.length) + '-';
+      result += value.slice(startString.length) + '-'; // eslint-disable-line no-param-reassign
     }
   });
   return result.slice(0, -1);
