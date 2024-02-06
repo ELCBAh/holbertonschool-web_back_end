e36d86c7391470a82166b883a548e063e9f41c4c
ELCBAh <6211@holbertonstudents.com>
2024-02-06 01:28:39 +0000

    fixed output error execution not successful

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -5,7 +5,7 @@ const createInt8TypedArray = (length, position, value) => {
   const buffer = new ArrayBuffer(length);
   const view = new Int8Array(buffer);
   view[position] = value;
-  return buffer;
+  return new DataView(buffer);
 };
 
 export default createInt8TypedArray;
