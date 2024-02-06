b4bdddc2873258fd99fde6cfefadbdccb497e21c
ELCBAh <6211@holbertonstudents.com>
2024-02-06 01:52:37 +0000

    fixed lint errors in clena_set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 const cleanSet = (set, startString) => {
-  let result = "";
+  let result = '';
 
   set.forEach((value) => {
     if (value.startsWith(startString)) {
-      result += value.slice(startString.length) + "-";
+      result += value.slice(startString.length) + '-';
     }
   });
   return result.slice(0, -1);
-}
+};
 
 export default cleanSet;
